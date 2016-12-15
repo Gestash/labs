335f14673721961c0766eee5b45c9098f40ea8e1
DESKTOP-KPSMLLC\user <shipunovakseniya@mail.ru>
2016-12-15 10:08:22 +0700

    Исправила сохранение и открытие, следущий шаг планирую сделать вывод EXIF

--- Graphic Editor/Graphic Editor/MainForm.cs
+++ Graphic Editor/Graphic Editor/MainForm.cs
@@ -16,31 +16,68 @@ namespace Graphic_Editor
             InitializeComponent();
         }
 
-        private void сохранитьToolStripMenuItem1_Click(object sender, EventArgs e)
-        {
-            Bitmap bmpSave = (Bitmap)pictureBox1.Image;
-            SaveFileDialog sfd = new SaveFileDialog();
-            if (sfd.ShowDialog() == DialogResult.OK)
-
-                bmpSave.Save(sfd.FileName, pictureBox1.Image, ImageFormat.Bmp);
-        }
-
+        public static Bitmap image;
+        public static string full_name_of_image = "\0";
+        public static UInt32[,] pixel;
+       
         private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
-                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            open_dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            if (open_dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
                 {
-                    System.IO.FileStream fs = new System.IO.FileStream(openFileDialog1.FileName, System.IO.FileMode.Open);
-                    System.Drawing.Image img = System.Drawing.Image.FromStream(fs);
-                    fs.Close();
-                    this.pictureBox1.Size = img.Size;
-                    pictureBox1.Image = img;
-                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-                    pictureBox1.Invalidate();
-
+                    full_name_of_image = open_dialog.FileName;
+                    image = new Bitmap(open_dialog.FileName);
+                    //this.pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    this.Width = image.Width + 40;
+                    this.Height = image.Height + 75;
+                    this.pictureBox1.Size = image.Size;
+                    pictureBox1.Image = image;
+                    pictureBox1.Invalidate(); //????
+                    //получение матрицы с пикселями
+                    pixel = new UInt32[image.Height, image.Width];
+                    for (int y = 0; y < image.Height; y++)
+                        for (int x = 0; x < image.Width; x++)
+                            pixel[y, x] = (UInt32)(image.GetPixel(x, y).ToArgb());
                 }
+                catch
+                {
+                    full_name_of_image = "\0";
+                    DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
             
         }
+        private void сохранитьToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                //string format = full_name_of_image.Substring(full_name_of_image.Length - 4, 4);
+                SaveFileDialog savedialog = new SaveFileDialog();
+                savedialog.Title = "Сохранить картинку как...";
+                savedialog.OverwritePrompt = true;
+                savedialog.CheckPathExists = true;
+                savedialog.Filter = "Image Files(*.BMP)|*.BMP|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)|*.GIF|Image Files(*.PNG)|*.PNG|All files (*.*)|*.*";
+                savedialog.ShowHelp = true;
+                if (savedialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        image.Save(savedialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
 
     }
 }
